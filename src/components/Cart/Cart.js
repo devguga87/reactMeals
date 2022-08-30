1fc1a336480ff78647a61078577e5376c8f0edb9
Gustavo Seabra <devguga87@gmail.com>
2022-08-29 22:51:06 -0300

    submit and sending cart data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -25,6 +25,16 @@ const Cart = (props) => {
     setIsCheckout(true)
   }
 
+  const submitOrderHandler = (userData) => {
+    fetch('https://react-http-fdcaa-default-rtdb.firebaseio.com/orders.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items
+      })
+    })
+  }
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
@@ -47,7 +57,7 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />}
       {!isCheckout && (
         <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
